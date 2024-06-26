82a8baed4e414260977484baf08a114ef5645ca2
Ghost Coding <danielvdberg@proton.me>
2024-06-26 15:45:00 +0200

    commiting basic changes for successful  deployment

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
-  ignoredRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhooks/clerk(.*)"],
   publicRoutes: ["/"],
   
 });
